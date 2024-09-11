317f9a0728f28eedb2be71cd4ee7dd9888447fae
曹钰 <naritashin@icloud.com>
2024-09-11 10:45:49 +0800

    feat: user info

--- packages/portal-pc/src/layout/components/TopBar/User.vue
+++ packages/portal-pc/src/layout/components/TopBar/User.vue
@@ -1,12 +1,14 @@
 <template>
-  <div
-    class="avatar-container"
-  >
-    <el-popover popper-class="user-popover" :offset="15" placement="bottom-end" :width="400" trigger="hover">
+  <div class="avatar-container">
+    <el-popover
+      popper-class="user-popover"
+      placement="bottom-end"
+      :offset="15"
+      :width="400"
+      trigger="hover"
+    >
       <template #reference>
-        <div
-          class="avatar-wrapper"
-        >
+        <div class="avatar-wrapper">
           <el-avatar
             :size="38"
             :src="user.avatar"
@@ -14,24 +16,23 @@
 
           <div class="flex flex-col">
             <div>
-              <div
-                class="user-code"
-              >
-                user2024090411548745
-              </div>
+              <div class="user-code">{{ user.uid }}</div>
             </div>
             <div class="flex items-center">
               <div
-                class="nick-name"         
+                class="nick-name"
                 :title="`@${user.nickName}`"
               >
                 @{{ user.nickName }}
               </div>
-              <div style="border:1px solid rgba(224, 255, 49, 0.5)" class="px-3 py-1 text-[#e1ff01] rounded-full text-xs">
+              <div
+                style="border: 1px solid rgba(224, 255, 49, 0.5)"
+                class="rounded-full px-3 py-1 text-xs text-[#e1ff01]"
+              >
                 <CountTo
                   suffix="pts"
                   :decimals="0"
-                  :end-val="25500"
+                  :end-val="user.points"
                 />
               </div>
             </div>
@@ -40,9 +41,7 @@
       </template>
       <template #default>
         <div>
-          <div
-            class="flex items-center p-5 avatar-wrapper2"
-          >
+          <div class="avatar-wrapper2 flex items-center p-5">
             <el-avatar
               :size="70"
               :src="user.avatar"
@@ -50,11 +49,7 @@
 
             <div class="flex flex-col">
               <div>
-                <div
-                  class="user-code"
-                >
-                  user2024090411548745
-                </div>
+                <div class="user-code">{{ user.uid }}</div>
               </div>
               <div class="flex items-center">
                 <div
@@ -66,26 +61,26 @@
               </div>
             </div>
           </div>
-          <el-divider style="border-color: rgba(0, 0, 0, 0.3);" />
+          <el-divider style="border-color: rgba(0, 0, 0, 0.3)" />
           <div>
-            <div 
-              v-for="(item, index) in infoList" 
-              :key="index" 
-              class="text-[#000] text-[16px] font-semibold flex items-center mt-2 pl-5 hover:opacity-75 cursor-pointer"
+            <div
+              v-for="(item, index) in infoList"
+              :key="index"
+              class="mt-2 flex cursor-pointer items-center pl-5 text-[16px] font-semibold text-[var(--el-text-color-primary)] hover:opacity-75"
               @click="toPage(item)"
             >
               <svg-icon
                 :name="item.icon"
-                class="mr-2 text-[24px] "
+                class="mr-2 text-[24px]"
               />
               <span>
                 {{ item.text }}
               </span>
-              <div 
-                v-if="item.id===2"
-                class="px-3 text-[#fff] bg-black rounded-full text-xs ml-4 h-7 leading-7"
+              <div
+                v-if="item.id === 2"
+                class="ml-4 h-7 rounded-full bg-black px-3 text-xs leading-7 text-[var(--el-text-color-regular)]"
               >
-                {{ 25500 }} pts
+                {{ user.points }} pts
               </div>
             </div>
           </div>
@@ -102,46 +97,47 @@ import useUserStore from '@/store/modules/user.js';
 const router = useRouter();
 const user = useUserStore();
 
-import useLoginStore from '@/store/modules/login'
+import useLoginStore from '@/store/modules/login';
 const useLogin = useLoginStore();
 
-const infoList =ref([
+const infoList = ref([
   {
-    id:1,
-    icon:'profile',
+    id: 1,
+    icon: 'profile',
     text: 'View Profile',
     path: '/user'
-  },{
-    id:2,
-    icon:'point',
+  },
+  {
+    id: 2,
+    icon: 'point',
     text: 'MojoGogo Points',
     path: '/tasks'
-  },{
-    id:3,
-    icon:'refer',
+  },
+  {
+    id: 3,
+    icon: 'refer',
     text: 'Refer friends',
     path: '/refer'
   },
   {
-    id:4,
-    icon:'logout',
+    id: 4,
+    icon: 'logout',
     text: 'Log out',
     path: ''
   }
-])
+]);
 
 onBeforeMount(() => {
   useUserStore().updateSysInfo();
 });
 
-
 // skip page
 const toPage = (item) => {
-  console.log(item,'item999')
-   if(item.id===4){
-    logoutHandle()
+  console.log(item, 'item999');
+  if (item.id === 4) {
+    logoutHandle();
   } else {
-    router.push({ path: item.path })
+    router.push({ path: item.path });
   }
 };
 
@@ -157,104 +153,94 @@ const logoutHandle = async () => {
       const auth = getAuth();
       await signOut(auth);
       await user.logOut();
-      useLogin.toLoginOut()
+      useLogin.toLoginOut();
       router.push({ path: '/home' });
     }
   } catch (e) {
     console.log(e);
   }
 };
-
 </script>
 
 <style lang="scss" scoped>
-  .avatar-wrapper {
-    display: flex;
-    align-items: center;
-    height: 44px;
-    padding: 0 10px;
-    border-radius: 8px;
+.avatar-wrapper {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 10px;
+  border-radius: 8px;
+  cursor: pointer;
+
+  .user-avatar {
     cursor: pointer;
-
-    .user-avatar {
-      cursor: pointer;
-      width: 30px;
-      height: 30px;
-      border-radius: 50%;
-    }
-
-    .nick-name {
-      margin: 0 10px;
-      font-size: 13px;
-      font-style: normal;
-      font-weight: 500;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      color: rgba(255, 255, 255, 0.70);
-
-    }
-    .user-code {
-      // width: 144px;
-      margin: 0 10px 6px 10px;
-      font-size: 15px;
-      font-style: normal;
-      font-weight: 600;
-      overflow: hidden;
-      color: var(--el-color-primary);
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
-    i {
-      cursor: pointer;
-      font-size: 14px;
-    }
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
   }
-  .avatar-wrapper2 {
-    align-items: center;
-    height: 70px;
-    border-radius: 8px;
 
-    .user-avatar {
-      cursor: pointer;
-      width: 60px;
-      height: 60px;
-      border-radius: 50%;
-    }
-
-    .nick-name {
-      margin: 0 0 6px 10px;
-      color: rgba(0, 0, 0, 0.70);
-      font-family: Inter;
-      font-size: 15px;
-      font-style: normal;
-      font-weight: 500;
-
-    }
-    .user-code {
-      margin: 0 0 0 10px;
-      font-size: 19px;
-      font-style: normal;
-      font-weight: 600;
-      overflow: hidden;
-      color: #1C1C21;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
-    i {
-      cursor: pointer;
-      font-size: 14px;
-    }
+  .nick-name {
+    margin: 0 10px;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 500;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: rgba(255, 255, 255, 0.7);
   }
+  .user-code {
+    max-width: 200px;
+    margin: 0 10px 6px 10px;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 600;
+    overflow: hidden;
+    color: var(--el-color-primary);
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  i {
+    cursor: pointer;
+    font-size: 14px;
+  }
+}
+.avatar-wrapper2 {
+  align-items: center;
+  border-radius: 8px;
+
+  .user-avatar {
+    cursor: pointer;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  .nick-name {
+    margin: 0 0 6px 10px;
+    color: var(--el-text-color-secondary);
+    font-size: 15px;
+  }
+  .user-code {
+    max-width: 216px;
+    margin: 0 0 0 10px;
+    font-size: 19px;
+    font-weight: 600;
+    color: var(--el-text-color-primary);
+  }
+
+  i {
+    cursor: pointer;
+    font-size: 14px;
+  }
+}
 </style>
 
 <style lang="scss">
-.user-popover{
+.user-popover {
   width: 360px !important;
   border: none !important;
   border-radius: 20px !important;
-  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.30) !important;
-} 
+  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.3) !important;
+}
 </style>
--- packages/portal-pc/src/store/modules/user.js
+++ packages/portal-pc/src/store/modules/user.js
@@ -1,53 +1,55 @@
-import { welcomeAccess } from '@gptx/base/api/login'
-import { setToken, removeToken, setTokenExpire} from '@gptx/base/utils/auth'
-import defAva from '@/assets/logo/avatar-default.svg'
+import { welcomeAccess } from '@gptx/base/api/login';
+import { removeToken, setToken, setTokenExpire } from '@gptx/base/utils/auth';
+import defAva from '@/assets/logo/avatar-default.svg';
 
-const useUserStore = defineStore(
-  'user',
-  {
-    state: () => ({
-      nickName: '',
-      avatar: '',
-      email: '',
-      phoneNumber: '',
-    }),
-    actions: {
-      loginOthers(authUserInfo) {
-        setToken(authUserInfo.accessToken)
-        setTokenExpire(authUserInfo.stsTokenManager.expirationTime)
-        console.log(authUserInfo,'loginOthers')
-        return Promise.resolve()
-      },
-      updateUserInfo (key, value) {
-        this[key] = value
-      },
-      logOut () {
-        return new Promise((resolve) => {
-          removeToken()
-          localStorage.removeItem('user')
-          localStorage.removeItem('humanVerified')
-          resolve()
+const useUserStore = defineStore('user', {
+  state: () => ({
+    nickName: '',
+    avatar: '',
+    email: '',
+    uid: '',
+    points: 0,
+    referalCode: ''
+  }),
+  actions: {
+    loginOthers(authUserInfo) {
+      setToken(authUserInfo.accessToken);
+      setTokenExpire(authUserInfo.stsTokenManager.expirationTime);
+      console.log(authUserInfo, 'loginOthers');
+      return Promise.resolve();
+    },
+    updateUserInfo(key, value) {
+      this[key] = value;
+    },
+    logOut() {
+      return new Promise((resolve) => {
+        removeToken();
+        localStorage.removeItem('user');
+        localStorage.removeItem('humanVerified');
+        resolve();
+      });
+    },
+    updateSysInfo() {
+      welcomeAccess()
+        .then((res) => {
+          console.log(res, 'res999');
+          if (res.data && res.data.user) {
+            const user = res.data.user;
+            const avatar = user.avatar === '' || user.avatar == null ? defAva : user.avatar;
+            this.nickName = user.nickname; // 昵称
+            this.avatar = avatar; // 头像
+            this.uid = user.uid || ''
+            this.email = user.email || '';
+            this.points = user.points || 0;
+            this.referalCode = user.referal_code || '';
+            localStorage.setItem('user', JSON.stringify(res.data.user));
+          }
         })
-      },
-      updateSysInfo () {
-          welcomeAccess().then((res) => {
-              console.log(res,'res999')
-              if (res.data && res.data.user) {
-                const user = res.data.user
-                const avatar = (user.avatar === '' || user.avatar == null) ? defAva : user.avatar
-                this.nickName = user.nickname// 昵称
-                this.avatar = avatar // 头像
-                // this.phoneNumber = user.phone_number || ''
-                this.email = user.email || ''
-                localStorage.setItem('user', JSON.stringify(res.data.user));
-              }
-            }
-          ).catch((err) => {
-            console.log(err,'err')
-          });
-      }
+        .catch((err) => {
+          console.log(err, 'err');
+        });
     }
   }
-)
+});
 
-export default useUserStore
+export default useUserStore;
